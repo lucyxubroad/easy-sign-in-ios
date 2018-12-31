0bee62765b5eb742339953c90400f6fdf8ed35db
Xu, Lucy L <lxu@mitre.org>
2018-12-31 10:59:56 -0500

    Initial Commit

---
+++ SignIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignIn
+//
+//  Created by Xu, Lucy L. on 12/31/18.
+//  Copyright © 2018 Xu, Lucy L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
